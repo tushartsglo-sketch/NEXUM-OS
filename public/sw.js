2fd3c4a9efb5db8002af9d41f9305cff3d601e65
tushartsglo-sketch <tushartsglo@gmail.com>
2026-09-25 03:06:51 +0600

    Add notification click service worker

---
+++ public/sw.js
@@ -0,0 +1,1 @@
+self.addEventListener("notificationclick",event=>{event.notification.close();const url=event.notification.data?.url||"/tasks";event.waitUntil(clients.matchAll({type:"window",includeUncontrolled:true}).then(list=>{for(const client of list){if("focus" in client){client.navigate(url);return client.focus()}}return clients.openWindow(url)}))});
